c14aee03602b560e7d3d8e9ad8d4fc4846211020
MaeganW <maeganwomble@gmail.com>
2019-01-25 10:41:32 -0600

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AnimeJs-Demo
+
+### Useful Sources
+* https://www.sitepoint.com/animating-the-dom-with-anime-js/
---
+++ index.js
@@ -0,0 +1,8 @@
+var bouncingBall = anime({
+	targets: '.ball',
+	translateY: '50vh',
+	duration: 300,
+	loop: true,
+	direction: 'alternate',
+	easing: 'easeInCubic'
+});
